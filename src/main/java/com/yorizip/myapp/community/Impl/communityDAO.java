60ac5dae011b96fbfc5bf1b6e0c7c817d3997d11
sh <whtjdgh1126@naver.com>
2024-09-01 13:41:44 +0900

    마지막

--- src/main/java/com/yorizip/myapp/community/Impl/communityDAO.java
+++ src/main/java/com/yorizip/myapp/community/Impl/communityDAO.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
